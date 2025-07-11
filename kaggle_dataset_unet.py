86281d8e09ae8ee76d8c3b24b0f7486fe7141496
curtis-shorts <curtis.shorts@queensu.ca>
2025-07-10 23:31:16 -0400

    fixed caesar python env for all models

---
+++ kaggle_dataset_unet.py
@@ -0,0 +1,13 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("mateuszbuda/lgg-mri-segmentation")
+
+print("Path to dataset files:", path)
+
+# Create ~/.kaggle/kaggle.json:
+#   mkdir ~/.kaggle
+#   echo '{"username":"curtis12345","key":"cc37227e37f67396fac644906cf45c35"}' > ~/.kaggle/kaggle.json
+# Link the downloaded directory to where the benchmark wants it:
+#   ln -s ~/.cache/kagglehub/datasets/mateuszbuda/lgg-mri-segmentation/versions/2 ~/.cache/mldata/lgg_segmentation
+
